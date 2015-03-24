5bbcd1a800df89b0d92aa122f223f2a63f56ab48
Gary N Griswold <gary@shortsands.com>
2015-03-24 16:32:51 -0400

    Added Dialog and StatusBar plugins.

--- UnitTestCSS1/platforms/ios/www/cordova_plugins.js
+++ UnitTestCSS1/platforms/ios/www/cordova_plugins.js
@@ -1,7 +1,25 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.statusbar": "0.1.10"
+}
 // BOTTOM OF METADATA
 });
