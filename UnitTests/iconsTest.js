0db12d48aa16f5d96cf6f889f06203f948b136d9
Gary N Griswold <gary@shortsands.com>
2015-06-11 01:06:21 -0400

    Added drawSendIcon.js

--- UnitTests/iconsTest.js
+++ UnitTests/iconsTest.js
@@ -4,16 +4,25 @@
 function iconsTest() {
 	var layoutStyle = 'display: inline-block; border: solid';
 
-	var sendBtn = document.createElement('div');
-	sendBtn.setAttribute('style', layoutStyle);
-	document.body.appendChild(sendBtn);
+	//var sendBtn = document.createElement('div');
+	//sendBtn.setAttribute('style', layoutStyle);
+	//document.body.appendChild(sendBtn);
 	var sendIcon = drawSendIcon(200, '#777777');
-	sendBtn.appendChild(sendIcon);
+	placeIcon(sendIcon);
+	//sendBtn.appendChild(sendIcon);
 
-	var questionsBtn = document.createElement('div');
-	questionsBtn.setAttribute('style', layoutStyle);
-	document.body.appendChild(questionsBtn);
+	//var questionsBtn = document.createElement('div');
+	//questionsBtn.setAttribute('style', layoutStyle);
+	//document.body.appendChild(questionsBtn);
 	var questionsIcon = drawQuestionsIcon(200, '#777777');
-	questionsBtn.appendChild(questionsIcon);
+	placeIcon(questionsIcon);
+	//questionsBtn.appendChild(questionsIcon);
+
+	function placeIcon(icon) {
+		var button = document.createElement('div');
+		button.setAttribute('style', layoutStyle);
+		document.body.appendChild(button);
+		button.appendChild(icon);		
+	}
 }
 
