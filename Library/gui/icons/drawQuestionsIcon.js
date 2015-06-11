fec498cb803e05e8b1b29a3d12e0e4c461be180c
Gary N Griswold <gary@shortsands.com>
2015-06-11 05:25:54 -0400

    Adjust the questions icon to be smaller.

--- Library/gui/icons/drawQuestionsIcon.js
+++ Library/gui/icons/drawQuestionsIcon.js
@@ -7,34 +7,36 @@ function drawQuestionsIcon(hite, color) {
 
 	var canvas = document.createElement('canvas');
 	canvas.setAttribute('height', hite);
-	canvas.setAttribute('width', hite * widthDiff);
+	canvas.setAttribute('width', hite * 1.2);
 	var graphics = canvas.getContext('2d');
 
-	graphics.beginPath();
-	drawOval(graphics);
-	drawArc(graphics, hite);
-	graphics.fillStyle = color;
-   	graphics.fill();
+	drawOval(graphics, hite * 0.72);
+	drawArc(graphics, hite * 0.72);
 	return(canvas);
 
-	function drawOval(graphics) {
+	function drawOval(graphics, hite) {
     	var centerX = 0;
     	var centerY = 0;
-    	var radius = hite * 0.45;
+    	var radius = hite * 0.5;
 
+		graphics.beginPath();
     	graphics.save();
-    	graphics.translate(canvas.width * 0.5, canvas.height * 0.45);
+    	graphics.translate(canvas.width * 0.5, canvas.height * 0.5);
     	graphics.scale(widthDiff, 1);
     	graphics.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
     	graphics.restore();
+    	graphics.fillStyle = color;
+   		graphics.fill();
     }
     
     function drawArc(graphics, hite) {
-    	graphics.moveTo(hite * 0.28, hite);
-    	graphics.bezierCurveTo(hite * 0.44, hite, hite * 0.66, hite * 0.73, hite * 0.72, hite * 0.45);
-    	graphics.lineTo(hite * 0.94, hite * 0.45);
-    	graphics.bezierCurveTo(hite * 0.9, hite * 0.73, hite * 0.54, hite, hite * 0.28, hite);
-    	graphics.closePath();
+    	graphics.beginPath();
+    	graphics.moveTo(hite * 0.3, hite * 1.25);
+    	graphics.bezierCurveTo(hite * 0.6, hite * 1.2, hite * 0.65, hite * 1.1, hite * 0.7, hite * 0.9);
+    	graphics.lineTo(hite * 0.5, hite * 0.9);
+    	graphics.bezierCurveTo(hite * 0.5, hite * 1, hite * 0.5, hite * 1.1, hite * 0.3, hite * 1.25);
+    	graphics.fillStyle = color;
+   		graphics.fill();
     }
 }
 
