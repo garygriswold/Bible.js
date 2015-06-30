a8fa185001d1fc7acbaf44a39c9fe09455585f5f
Gary N Griswold <gary@shortsands.com>
2015-06-30 16:54:00 -0400

    wrote class deviceSettings.

---
+++ Library/util/deviceSettings.js
@@ -0,0 +1,38 @@
+/**
+ * This class accesses the device locale and language information using the globalization plugin
+ * and the versions, platform and model of the device plugin, and the network status.
+ */
+var deviceSettings = {
+    prefLanguage: function(callback) {
+        navigator.globalization.getPreferredLanguage(onSuccess, onError);
+
+        function onSuccess(pref) {
+            callback(pref.value);
+        }
+        function onError() {
+            callback('en-XX');
+        }
+    },
+    platform: function() {
+        return(device.platform);
+    },
+    model: function() {
+        return(device.model);
+    },
+    uuid: function() {
+        return(device.uuid);
+    },
+    osVersion: function() {
+        return(device.version);
+    },
+    cordovaVersion: function() {
+        return(device.cordova);
+    },
+    connectionType: function() {
+        return(navigator.connection.type);
+    },
+    hasConnection: function() {
+        var type = navigator.connection.type;
+        return(type !== 'none' && type !== 'unknown'); // not sure of correct value for UNKNOWN
+    }
+}
