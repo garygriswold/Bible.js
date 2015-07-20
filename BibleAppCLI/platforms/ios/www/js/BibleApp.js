53aa70f9a26fff25a1b86e1261c43c3b5ce2e868
Gary N Griswold <gary@shortsands.com>
2015-07-20 14:23:29 -0400

    New files because of anderoid run.

--- BibleAppCLI/platforms/ios/www/js/BibleApp.js
+++ BibleAppCLI/platforms/ios/www/js/BibleApp.js
@@ -115,10 +115,6 @@ AppViewController.prototype.begin = function(develop) {
 			that.searchView.hideView();
 			that.history.addEvent(event);
 		});
-//		document.body.addEventListener(BIBLE.CHG_HEADING, function(event) {
-//			console.log('caught set title event', JSON.stringify(event.detail.reference));
-//			that.statusBar.setTitle(event.detail.reference);
-//		});
 		document.body.addEventListener(BIBLE.SHOW_NOTE, function(event) {
 			that.codexView.showFootnote(event.detail.id);
 		});
@@ -161,7 +157,6 @@ CodexView.prototype.hideView = function() {
 	}
 };
 CodexView.prototype.showView = function(nodeId) {
-	console.log('Codex.show view', nodeId);
 	this.chapterQueue.splice(0);
 	var firstChapter = new Reference(nodeId);
 	firstChapter = this.tableContents.ensureChapter(firstChapter);
@@ -195,19 +190,15 @@ CodexView.prototype.showView = function(nodeId) {
 			that.scrollTo(firstChapter);
 			that.currentNodeId = firstChapter.nodeId;
 			that.addChapterInProgress = false;
-			console.log('add event listener SCROLL onScrollHandler');
 			document.addEventListener('scroll', onScrollHandler);
 		}
 	}
 	function onScrollHandler(event) {
-		console.log('found on scroll event', that.currentNodeId);
 		if (! that.addChapterInProgress && that.chapterQueue.length > 1) {
 			var ref = identifyCurrentChapter();
-			console.log('passed first test', ref.nodeId, that.currentNodeId);
 			if (ref.nodeId !== that.currentNodeId) {
-				console.log('passed second test');
 				that.currentNodeId = ref.nodeId;
-				//document.body.dispatchEvent(new CustomEvent(BIBLE.CHG_HEADING, { detail: { reference: ref }}));//expensive solution
+				document.body.dispatchEvent(new CustomEvent(BIBLE.CHG_HEADING, { detail: { reference: ref }}));//expensive solution
 			}
 			if (document.body.scrollHeight - (window.scrollY + window.innerHeight) <= window.innerHeight) {
 				that.addChapterInProgress = true;
