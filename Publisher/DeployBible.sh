5011a407c9150d3912d532bb4604aa8e18a19e74
Gary N Griswold <gary@shortsands.com>
2015-07-25 00:49:45 -0400

    Add Script to copy Bible to various projects.

---
+++ Publisher/DeployBible.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd "${HOME}/Library/Application Support/Publisher/databases/file__0/"
+pwd
+ls -l
+cp 2 "${HOME}/Library/Application Support/Publisher/databases/file__0/1"
+cp 2 $HOME/BibleApp/BibleAppCLI/www/WEB.word1
+
+echo "Remember to delete the copy of WEB.word1 in simulator's Library/LocalDatabase/Web.word1"
