c91ee0690417643790f3aa93ac268d361a100363
Gary N Griswold <gary@shortsands.com>
2015-08-08 22:56:26 -0400

    Able to display text in HTMLView in iOS. using NativeScript.

--- BibleAppTNS/app/CodexView.js
+++ BibleAppTNS/app/CodexView.js
@@ -1,26 +1,27 @@
 var IOError = require('./io/IOError');
 var DeviceDatabase = require("./io/DeviceDatabase");
 
-var observableModule = require("data/observable");
-var source = new observableModule.Observable();
-
 function pageLoaded(args) {
     var page = args.object;
-    page.bindingContext = source;
+    
+    var scrollViewModule = require('ui/scroll-view');
+    var scrollView = new scrollViewModule.ScrollView();
+    page.content = scrollView;
 
-    source.set("oneChapter", "Text set via binding");
+    var htmlViewModule = require("ui/html-view");
+    var htmlView = new htmlViewModule.HtmlView();
+    scrollView.content = htmlView;
+    iosVersion = htmlView.ios;
+    iosVersion.numberOfLines = 0;
+    htmlView.html = 'Hello World';
 
-    console.log('going to instantiate database');
     var database = new DeviceDatabase('WEB.word1');
     database.open(function(error) {
-    	console.log('returned from open');
     	if (error instanceof IOError) {
     		console.log('error found after open', error);
     		callback(error);
     	} else {
-    		console.log('going to do select');
             database.chapters.getChapters(['JHN:0', 'JHN:1', 'JHN:2', 'JHN:3', 'JHN:4'], function(results) {
-                console.log('back from select');
                 if (results instanceof IOError) {
                     console.log('Error found ' + results.message);
                     callback(results);
@@ -29,9 +30,12 @@ function pageLoaded(args) {
                         var row = results[i];
                         console.log(row[0]);
                     }
-                    var content = '<div>' + results[3][1] + '</div>';
-                    source.set("oneChapter", content);
-                    console.log('******* |' + source.oneChapter + '| *******');
+                    var content = String(results[3][1]);
+                    var sectionEnd = content.indexOf('</section>');
+                    content = content.substr(sectionEnd + 10);
+                    var content = '<div class="top">' + content + '</div>';
+                    htmlView.html = content;
+                    console.log('******* |' + content + '| *******');
                     callback();
                 }
             });
