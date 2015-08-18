a5efb5917691056bf011ddee212675b6a507db9d
Gary N Griswold <gary@shortsands.com>
2015-08-18 17:00:10 -0400

    Transition to ForgotView is working.

--- QAApp/www/Transition.js
+++ QAApp/www/Transition.js
@@ -4,6 +4,7 @@
 var TRANSITION = { EVENT: 'transition', IMMEDIATE: 'immediate', SLIDE_LEFT: 'slide-left', SLIDE_RIGHT: 'slide-right' };
 
 function dispatchTransition(fromView, toView, transType) {
+	console.log('inside dispatchTransition', fromView, toView, transType);
 	var evtDetails = { fromView: fromView, toView: toView, transType: transType };
 	var event = new CustomEvent(TRANSITION.EVENT, { detail: evtDetails });
 	document.body.dispatchEvent(event);
