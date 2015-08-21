4e7f7483ddd88aeb23d0f7a00cc4ab4ba20a394b
Gary N Griswold <gary@shortsands.com>
2015-08-21 00:52:07 -0400

    Finished coding the Router.js file.

--- Server/www/Router.js
+++ Server/www/Router.js
@@ -5,36 +5,80 @@
 var restify = require('restify');
 var serverOptions = {};
 var server = restify.createServer(serverOptions);
-server.pre(restify.pre.userAgentConnection()); // if UA is curl, close connection and remove Content-Length header.
+server.pre(restify.pre.userAgentConnection()); // if UA is curl, close connection.
 
 server.use(restify.bodyParser({
 	maxBodySize: 10000,
 	mapParams: true
 }));
 
-server.get('/version/:versionId', function(request, response, next) {
-	console.log('Download version transaction ', request.params.versionId);
-	response.send('Download Version TBD = ' + request.params.versionId);
+server.post('/user', function(request, response, next) {
+	console.log('Register a new user', request.params.username);
+	response.send(200, 'Register a new user ' + request.params.username);
 	return(next());
 });
 
-server.get('/locale/:localeId', function(request, response, next) {
-	console.log('Download Ethnologe info ', request.params.localeId);
-	response.send('Download Ethnologe info = ' + request.params.localeId);
+server.get('/version/:version', function(request, response, next) {
+	console.log('Download version transaction ', request.params.version);
+	response.send(200, 'Download Version TBD = ' + request.params.version);
+	return(next());
+});
+
+server.get('/locale/:locale', function(request, response, next) {
+	console.log('Download Ethnologe info ', request.params.locale);
+	response.send(200, 'Download Ethnologe info = ' + request.params.locale);
 	return(next());	
 });
 
 server.post('/question', function(request, response, next) {
-	console.log('POST question ' + request.params.question, request.params.hello);
-	response.send('POST Question ' + request.params.question, request.params.hello);
+	console.log('POST question ' + request.params.question);
+	response.send(200, 'POST Question ' + request.params.question + ' ' + request.params.hello);
 	return(next());	
 });
 
-server.get('/:name', function(request, response, next) {
-	response.send('hello world ' + request.params.name);
+server.get('/assign/:version', function(request, response, next) {
+	console.log('Assign Question ', request.params.version);
+	response.send(200, 'Assign Question ' + request.params.version);
 	return(next());
 });
+
+server.get('/return/:questionId', function(request, response, next) {
+	console.log('Return question ', request.params.questionId);
+	response.send(200, 'Return question ' + request.params.questionId);
+	return(next());
+});
+
+server.post('/send', function(request, response, next) {
+	console.log('Send response ', request.params.message);
+	response.send(200, 'Send response ' + request.params.message);
+	return(next());
+});
+
+server.post('/draft', function(request, response, next) {
+	console.log('Save Draft ' + request.params.message);
+	response.send(200, 'Save Draft ' + request.params.message);
+	return(next());
+});
+
+server.get('/draft/:draftId', function(request, response, next) {
+	console.log('Get Draft ' + request.params.draftId);
+	response.send(200, 'Get Draft ' + request.params.draftId);
+	return(next());
+});
+
+server.del('/draft/:draftId', function(request, response, next) {
+	console.log('Delete Draft ' + request.params.draftId);
+	response.send(200, 'Delete Draft ' + request.params.draftId);
+	return(next());
+});
+
+server.get(/^.*$/, function(request, response, next) {
+	console.log('No Valid Route', request.url);
+	response.send(404, 'No Valid Route for ' + request.url);
+	return(next());
+})
 
 server.listen(8080, function() {
 	console.log('listening on 8080');
 });
+
