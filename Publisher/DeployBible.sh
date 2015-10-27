46bb8ad3edc4a7d2b554f65b8327257aafaa6b1e
Gary N Griswold <gary@shortsands.com>
2015-10-27 15:08:24 -0400

    Change Publisher to output files with .db1 suffix.

--- Publisher/DeployBible.sh
+++ Publisher/DeployBible.sh
@@ -3,6 +3,6 @@ cd "${HOME}/Library/Application Support/Publisher/databases/file__0/"
 pwd
 ls -l
 cp 2 "${HOME}/Library/Application Support/BibleAppNW/databases/file__0/1"
-cp 2 $HOME/BibleApp/BibleAppCLI/www/WEB.word1
+cp 2 $HOME/BibleApp/BibleAppCLI/www/WEB.db1
 
-echo "Remember to delete the copy of WEB.word1 in simulator's Library/LocalDatabase/Web.word1"
+echo "Remember to delete the copy of WEB.word1 in simulator's Library/LocalDatabase/WEB.db1"
