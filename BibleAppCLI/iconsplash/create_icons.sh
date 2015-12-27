2778fa7029f60aeb98be0dbd098c03227e141b08
Gary N Griswold <gary@shortsands.com>
2015-12-27 02:12:33 -0500

    Wrote script to create icons.

--- BibleAppCLI/iconsplash/create_icons.sh
+++ BibleAppCLI/iconsplash/create_icons.sh
@@ -5,23 +5,24 @@ export SOURCE_LG=Bible_blue_512.png
 export IOS_TARGET=../platforms/ios/Bible.js/Resources/icons
 export AND_TARGET=../platforms/android/res
 
-convert $SOURCE_IMG -resize 640x1136\! 	$IOS_TARGET/Default-568h@2x~iphone.png
-convert $SOURCE_IMG -resize 750x1334\! 	$IOS_TARGET/Default-667h.png
-convert $SOURCE_IMG -resize 1242x2208\! $IOS_TARGET/Default-736h.png
-convert $SOURCE_IMG -resize 2208x1242\! $IOS_TARGET/Default-Landscape-736h.png
-convert $SOURCE_IMG -resize 2048x1536\! $IOS_TARGET/Default-Landscape@2x~ipad.png
-convert $SOURCE_IMG -resize 1024x768\! 	$IOS_TARGET/Default-Landscape~ipad.png
-convert $SOURCE_IMG -resize 1536x2048\! $IOS_TARGET/Default-Portrait@2x~ipad.png
-convert $SOURCE_IMG -resize 768x1024\! 	$IOS_TARGET/Default-Portrait~ipad.png
-convert $SOURCE_IMG -resize 640x960\! 	$IOS_TARGET/Default@2x~iphone.png
-convert $SOURCE_IMG -resize 320x480\! 	$IOS_TARGET/Default~iphone.png
+convert $SOURCE_SM -resize 40x40 	$IOS_TARGET/icon-40.png
+convert $SOURCE_SM -resize 80x80 	$IOS_TARGET/icon-40@2x.png
+convert $SOURCE_SM -resize 50x50 	$IOS_TARGET/icon-50.png
+convert $SOURCE_SM -resize 100x100	$IOS_TARGET/icon-50@2x.png
+convert $SOURCE_SM -resize 60x60 	$IOS_TARGET/icon-60.png
+convert $SOURCE_SM -resize 120x120 	$IOS_TARGET/icon-60@2x.png
+convert $SOURCE_LG -resize 180x180	$IOS_TARGET/icon-60@3x.png
+convert $SOURCE_SM -resize 72x72 	$IOS_TARGET/icon-72.png
+convert $SOURCE_SM -resize 144x144 	$IOS_TARGET/icon-72@2x.png
+convert $SOURCE_SM -resize 76x76 	$IOS_TARGET/icon-76.png
+convert $SOURCE_SM -resize 152x152 	$IOS_TARGET/icon-76@2x.png
+convert $SOURCE_SM -resize 29x29 	$IOS_TARGET/small.png
+convert $SOURCE_SM -resize 58x58 	$IOS_TARGET/small@2x.png
+convert $SOURCE_SM -resize 57x57 	$IOS_TARGET/icon.png
+convert $SOURCE_SM -resize 114x114 	$IOS_TARGET/icon@2x.png
 
-convert $SOURCE_IMG -resize 800x480\!	$AND_TARGET/drawable-land-hdpi/screen.png
-convert $SOURCE_IMG -resize 320x200\!	$AND_TARGET/drawable-land-ldpi/screen.png
-convert $SOURCE_IMG -resize 480x320\!	$AND_TARGET/drawable-land-mdpi/screen.png
-convert $SOURCE_IMG -resize 1280x720\!	$AND_TARGET/drawable-land-xhdpi/screen.png
-
-convert $SOURCE_IMG -resize 480x800\!	$AND_TARGET/drawable-port-hdpi/screen.png
-convert $SOURCE_IMG -resize 200x320\!	$AND_TARGET/drawable-port-ldpi/screen.png
-convert $SOURCE_IMG -resize 320x480\!	$AND_TARGET/drawable-port-mdpi/screen.png
-convert $SOURCE_IMG -resize 720x1280\!	$AND_TARGET/drawable-port-xhdpi/screen.png
+convert $SOURCE_SM -resize 96x96	$AND_TARGET/drawable/icon.png
+convert $SOURCE_SM -resize 72x72	$AND_TARGET/drawable-hdpi/icon.png
+convert $SOURCE_SM -resize 36x36	$AND_TARGET/drawable-ldpi/icon.png
+convert $SOURCE_SM -resize 48x48	$AND_TARGET/drawable-mdpi/icon.png
+convert $SOURCE_SM -resize 96x96	$AND_TARGET/drawable-xhdpi/icon.png
