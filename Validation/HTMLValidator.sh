74aa792b0292f0697eb91d345f6372bd75b22c21
Gary N Griswold <gary@shortsands.com>
2016-05-26 17:44:56 -0400

    HTML Validation is working with only some single space inconsistencies.

---
+++ Validation/HTMLValidator.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+	echo "Usage: HTMLValidator.sh VERSION";
+	exit 1;
+fi
+
+VERSION=$1;
+DB_PATH=../../DBL/3prepared/${VERSION}.db;
+
+echo ${DB_PATH}
+
+sqlite3 ${DB_PATH} <<END_SQL
+.output output/verses.txt
+select reference, html from verses;
+END_SQL
+
+echo \"use strict\"\; > temp.js
+cat ../Library/model/meta/Canon.js >> temp.js
+cat ../Library/xml/XMLTokenizer.js >> temp.js
+cat js/HTMLValidator.js >> temp.js
+
+node temp.js $1
+
+diff output/chapters.txt output/verses.txt
