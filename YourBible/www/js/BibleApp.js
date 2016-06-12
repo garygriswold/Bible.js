0bf72bedf109cf69800d4f990b47228de2f12ad5
Gary N Griswold <gary@shortsands.com>
2016-06-12 00:21:41 -0400

    Added 4 styles for the Chinese versions.

--- YourBible/www/js/BibleApp.js
+++ YourBible/www/js/BibleApp.js
@@ -14,14 +14,15 @@ AppInitializer.prototype.begin = function() {
     	var fileMover = new FileMover(settingStorage);
     	console.log('START MOVE FILES');
 		fileMover.copyFiles(function() {
-			console.log('DONE WITH MOVE FILES');    
+			console.log('DONE WITH MOVE FILES');
+			settingStorage.initSettings();   
 		    settingStorage.getCurrentVersion(function(versionFilename) {
 				if (versionFilename == null) {
 					deviceSettings.prefLanguage(function(locale) {
 						var parts = locale.split('-');
 						versionFilename = settingStorage.defaultVersion(parts[0]);
 						settingStorage.setCurrentVersion(versionFilename);
-						settingStorage.initSettings();
+						//settingStorage.initSettings();
 						changeVersionHandler(versionFilename);
 					});
 				} else {
