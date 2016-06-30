ea8450d6c8bfc16e5d0c8feafc958207b6a589ad
Gary N Griswold <gary@shortsands.com>
2016-06-30 07:24:06 -0400

    Temporary fix version 1.1.5 making NMV the default.

--- YourBible/www/js/SettingStorageInitSettings.js
+++ YourBible/www/js/SettingStorageInitSettings.js
@@ -1,6 +1,6 @@
 SettingStorage.prototype.initSettings = function() {
 	this.setVersion("ARBVDPD", "ARBVDPD.db");
-	this.setVersion("CUVSPD", "CUVSPD.db");
+	//this.setVersion("CUVSPD", "CUVSPD.db");
 	this.setVersion("KJVPD", "KJVPD.db");
 	this.setVersion("NMV", "NMV.db");
 	this.setVersion("WEB", "WEB.db");
@@ -11,6 +11,6 @@ SettingStorage.prototype.defaultVersion = function(lang) {
 		case "zh": return("CUVSPD.db");
 		case "fa": return("NMV.db");
 		case "en": return("WEB.db");
-		default: return("WEB.db");
+		default: return("NMV.db");
 	}
 };
