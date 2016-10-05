e18e3b3b995b093f3b61f3a748c20ddd81147a0d
Gary N Griswold <gary@shortsands.com>
2016-10-05 03:17:00 -0400

    Resequence codes in StylesUseBuilder.

--- Library/manufacture/StyleUseBuilder.js
+++ Library/manufacture/StyleUseBuilder.js
@@ -9,14 +9,24 @@ StyleUseBuilder.prototype.readBook = function(usxRoot) {
 	// This table is not populated from text of the Bible
 };
 StyleUseBuilder.prototype.loadDB = function(callback) {
-	var styles = [ 'book.id', 'para.ide', 'para.h', 'para.toc1', 'para.toc2', 'para.toc3', 'para.cl', 'para.rem',
-		'para.mt', 'para.mt1', 'para.mt2', 'para.mt3', 'para.ms', 'para.ms1', 'para.s', 'para.s1', 'para.s2', 'para.d', 'para.r',
-		'para.imt', 'para.io', 'para.io1', 'para.io2', 'para.ip', 'para.is',
-		'chapter.c', 'verse.v',
-		'para.p', 'para.m', 'para.b', 'para.mi', 'para.pi', 'para.pi1', 'para.li', 'para.li1', 'para.nb',
-		'para.sp', 'para.q', 'para.q1', 'para.q2', 'para.qc', 'para.qa',
-		'char.pn', 'char.wj', 'char.qs', 'char.add', 'char.nd', 'char.tl',
-		'note.f', 'note.x', 'char.xt', 'char.fr', 'char.ft', 'char.fv', 'char.fqa', 'char.xo' ];
+	var styles = [ 'book.id', 'para.ide', 'para.h', 'para.toc1', 'para.toc2', 'para.toc3', 'para.rem',
+		'para.imt', 'para.is',
+		'para.io', 'para.io1', 'para.io2',
+		'para.ip',
+		'chapter.c', 'para.cl', 'verse.v',
+		'para.p', 'para.m', 'para.pi', 'para.pi1', 'para.mi',
+		'para.nb', 'para.q', 'para.q1', 'para.q2', 'para.qc',
+		'char.qs', 'para.qa', 'para.b', 
+		'para.mt', 'para.mt1', 'para.mt2', 'para.mt3', 
+		'para.ms', 'para.ms1', 
+		'para.s', 'para.s1', 'para.s2', 'para.r', 'para.sp', 'para.d', 
+		'para.li', 'para.li1',
+		'char.qs',  
+		'note.f', 'char.ft', 'char.fr', 
+		'char.fqa', 'char.fv', 
+		'note.x', 'char.xt', 'char.xo'
+		'char.nd', 'char.tl','char.pn', 'char.wj', 'char.add' 
+		];
 	var array = [];
 	for (var i=0; i<styles.length; i++) {
 		var style = styles[i];
