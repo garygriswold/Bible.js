a14633ce430e3ac816c82ec650f089538b754164
Gary N Griswold <gary@shortsands.com>
2017-01-20 15:30:22 -0500

    Create ios VideoPlayer, but have not yet added code.

---
+++ Plugins/VideoPlayer/ios/VideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Gary Griswold on 1/20/17.
+//  Copyright © 2017 ShortSands. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
