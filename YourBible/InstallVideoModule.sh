e76eb01eef7fde39b1eecde5cf4e00a8d95ecda3
Gary N Griswold <gary@shortsands.com>
2017-05-14 02:55:22 -0400

    update console package.

--- YourBible/InstallVideoModule.sh
+++ YourBible/InstallVideoModule.sh
@@ -2,7 +2,6 @@
 
 cordova plugin remove com-shortsands-videoplayer
 cordova plugin add $HOME/ShortSands/BibleApp/Plugins/VideoPlayer
-cp build-extras.gradle platforms/android/
+cp plugins/com-shortsands-videoplayer/src/android/build-extras.gradle platforms/android/
 cordova prepare ios
 
-## change cp to pick it up from plugin, not app directory
