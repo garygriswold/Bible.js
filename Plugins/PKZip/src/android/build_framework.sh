20cefa94d71aeda8e08cd9b3f8d3a098efe9d49d
Gary N Griswold <gary@shortsands.com>
2017-06-26 22:49:17 -0400

    Moved the jar copy logic from build.gradle to build_framework.sh

--- Plugins/PKZip/src/android/build_framework.sh
+++ Plugins/PKZip/src/android/build_framework.sh
@@ -1,4 +1,15 @@
 #!/bin/sh -ve
 
-./gradlew deleteJars clean assemble check copyDebug copyRelse 
+LIBROOT=${HOME}/Library/Frameworks
+DEBUG_ROOT=${LIBROOT}/Debug-android
+RELSE_ROOT=${LIBROOT}/Release-android
+JAR=Zip.jar
+
+rm -f ${DEBUG_ROOT}/${JAR}
+rm -f ${RELSE_ROOT}/${JAR}
+
+./gradlew clean assemble check 
+
+cp app/build/intermediates/bundles/debug/classes.jar ${DEBUG_ROOT}/${JAR}
+cp app/build/intermediates/bundles/default/classes.jar ${RELSE_ROOT}/${JAR}
 
