4f528e916f02ae1dbdf9849497f6db3fcd0be44c
Gary N Griswold <gary@shortsands.com>
2017-06-27 20:10:16 -0400

    AWS have build_frameword.sh

---
+++ Plugins/AWS/src/android/build_framework.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -ve
+
+LIBROOT=${HOME}/Library/Frameworks
+DEBUG_ROOT=${LIBROOT}/Debug-android
+RELSE_ROOT=${LIBROOT}/Release-android
+JAR=AWS.jar
+
+rm -f ${DEBUG_ROOT}/${JAR}
+rm -f ${RELSE_ROOT}/${JAR}
+
+./gradlew clean assemble check 
+
+# Copy Jars to Frameworks
+cp app/build/intermediates/bundles/debug/classes.jar ${DEBUG_ROOT}/${JAR}
+cp app/build/intermediates/bundles/default/classes.jar ${RELSE_ROOT}/${JAR}
+
+# Copy Debug Jar to this project libs
+cp ${DEBUG_ROOT}/${JAR} app/libs
+
+# Copy Debug Jar to VideoPlayer project libs
+#cp ${DEBUG_ROOT}/${JAR} ../../../AWS/src/android/app/libs/
+
