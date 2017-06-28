42e761f92965d117dc8f1c3d0eaea77256d67cde
Gary N Griswold <gary@shortsands.com>
2017-06-27 23:34:01 -0400

    Put AWS Cordova Plugin classes into new structure.

---
+++ Plugins/AWS/src/android/plugin/UploadPluginFileListener.java
@@ -0,0 +1,32 @@
+package plugin;
+
+import android.util.Log;
+import com.shortsands.aws.UploadFileListener;
+import java.io.File;
+import org.apache.cordova.CallbackContext;
+/**
+ * Created by garygriswold on 5/22/17.
+ */
+
+public class UploadPluginFileListener extends UploadFileListener {
+
+    private static String TAG = "UploadPluginFileListener";
+    protected CallbackContext callbackContext;
+    
+    public UploadPluginFileListener(CallbackContext callbackContext) {
+        super();
+	    this.callbackContext = callbackContext;
+    }
+    
+    @Override
+    protected void onComplete(int id) {
+        super.onComplete(id);
+        this.callbackContext.success();
+    }
+    
+    @Override
+    public void onError(int id, Exception error) {
+	    super.onError(id, error);
+        this.callbackContext.error(error.toString() + " on " + this.file.getAbsolutePath());
+    }
+}
