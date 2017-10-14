d9a3c936ce2fe5447443a2dfdb6399eb25e2752a
Gary N Griswold <gary@shortsands.com>
2017-10-14 01:31:12 -0400

    Instantiate and use WKWebView.

--- Plugins/TextView/src/ios_TextView/TextView/ViewController.swift
+++ Plugins/TextView/src/ios_TextView/TextView/ViewController.swift
@@ -5,16 +5,31 @@
 //  Created by Gary Griswold on 10/14/17.
 //  Copyright © 2017 ShortSands. All rights reserved.
 //
-
+import Foundation
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://www.google.com")
+        let request = URLRequest(url: url!)
+        webView.load(request)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
