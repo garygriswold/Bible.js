32303c0f7e94c59a6d6e7b0a637d4a183d6bfd5f
Gary N Griswold <gary@shortsands.com>
2017-10-14 13:37:45 -0400

    Enable access of html from S3.

--- Plugins/TextView/src/ios_TextView/TextView/ViewController.swift
+++ Plugins/TextView/src/ios_TextView/TextView/ViewController.swift
@@ -5,15 +5,16 @@
 //  Created by Gary Griswold on 10/14/17.
 //  Copyright © 2017 ShortSands. All rights reserved.
 //
-import Foundation
 import UIKit
 import WebKit
+import AWS
 
 class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     
     var webView: WKWebView!
     
     override func loadView() {
+        AwsS3.region = "us-west-2"
         let webConfiguration = WKWebViewConfiguration()
         
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
@@ -24,17 +25,21 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let url = URL(string: "https://www.google.com")
-        let request = URLRequest(url: url!)
-        webView.load(request)
+        AwsS3.shared.preSignedUrlGET(s3Bucket: "text-" + AwsS3.region + "-shortsands",
+                                     s3Key: "ENGKJVT_04_JHN_003.html",
+                                     expires: 3600,
+                                     complete: { url in
+            print("URL is \(String(describing:url))")
+            if let goodUrl = url {
+                let request = URLRequest(url: goodUrl)
+                self.webView.load(request)
+            }
+        })
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
