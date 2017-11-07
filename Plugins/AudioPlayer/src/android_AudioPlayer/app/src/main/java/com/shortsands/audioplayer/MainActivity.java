6c8a870765d0a8572d5e5eff8623b71b5da341a5
Gary N Griswold <gary@shortsands.com>
2017-11-07 14:40:44 -0500

    Change App to restart properly on each restart.

--- Plugins/AudioPlayer/src/android_AudioPlayer/app/src/main/java/com/shortsands/audioplayer/MainActivity.java
+++ Plugins/AudioPlayer/src/android_AudioPlayer/app/src/main/java/com/shortsands/audioplayer/MainActivity.java
@@ -1,12 +1,9 @@
 package com.shortsands.audioplayer;
 
 import android.os.Bundle;
-//import android.support.design.widget.FloatingActionButton;
-//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-//import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -21,18 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        //fab.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View view) {
-        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        //                .setAction("Action", null).show();
-        //    }
-        //});
-
-        this.controller = new AudioBibleController(this);
-        this.controller.present();
     }
 
     @Override
@@ -61,16 +46,21 @@ public class MainActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         Log.d(TAG, "*** onResume is called.");
+        this.controller = new AudioBibleController(this);
+        this.controller.present();
+
     }
 
     /**
      * onPause is called here when the App is quit by using any of the three
-     * buttons at the bottom.
+     * buttons at the bottom, or the lock button.
      */
     @Override
     public void onPause() {
         super.onPause();
         Log.d(TAG, "*** onPause is called.");
-        this.controller.appHasExited();
+        if (this.controller != null) {
+            this.controller.appHasExited();
+        }
     }
 }
