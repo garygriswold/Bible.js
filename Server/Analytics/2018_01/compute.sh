af0e23b30f2c5a5e7ad4a09fb9b71ab7c03f522a
Gary N Griswold <gary@shortsands.com>
2018-01-23 12:23:10 -0500

    Finished Analytics report to Bible League Intl.

--- Server/Analytics/2018_01/compute.sh
+++ Server/Analytics/2018_01/compute.sh
@@ -19,7 +19,7 @@ GROUP BY version, osType;
 
 DROP TABLE IF EXISTS VersionByLanguage;
 CREATE TABLE VersionByLanguage AS
-SELECT v.version AS version, v.language AS langCode, l.englishName AS englishName
+SELECT v.version AS version, v.language AS langCode, l.englishName AS englishName, count(*) AS count
 FROM DownloadERV v, Language l
 WHERE v.language=l.langCode
 GROUP BY v.version, v.language;
@@ -32,6 +32,19 @@ GROUP BY version, year, month
 
 END_SQL1
 
-## OUTPUT TABLE VersionByCountryPivot
+# Pivot3: databaseName, tableName, xAxisColumn, yAxisColumn, value
+
+## Versions by Month -> VersionByMonth.csv
+node Pivot3 DownloadERV.db VersionByMonth version period count
+
+## Version by Country -> VersionByCountry.csv
 node Pivot3 DownloadERV.db VersionByCountry countryName version count
+
+## Version by Language -> VersionByLanguage.csv
+node Pivot3 DownloadERV.db VersionByLanguage englishName version count
+
+## Version by DeviceOS -> VersionByDeviceOS.csv
+node Pivot3 DownloadERV.db VersionByDeviceOS version osType count
+
+
 
