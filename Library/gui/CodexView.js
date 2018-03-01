8153c757fc2536292191ceb0545ffeafe578a537
Gary N Griswold <gary@shortsands.com>
2018-03-01 13:33:20 -0500

    Fix scrolling to always add a chapter above and a chapter below.

--- Library/gui/CodexView.js
+++ Library/gui/CodexView.js
@@ -1,7 +1,7 @@
 /**
 * This class contains user interface features for the display of the Bible text
 */
-var CODEX_VIEW = {BEFORE: 0, AFTER: 1, MAX: 100000, SCROLL_TIMEOUT: 200};
+var CODEX_VIEW = {BEFORE: 0, AFTER: 1, MAX: 100000, SCROLL_TIMEOUT: 250};
 
 function CodexView(chaptersAdapter, tableContents, headerHeight, copyrightView) {
 	this.chaptersAdapter = chaptersAdapter;
@@ -40,7 +40,7 @@ CodexView.prototype.showView = function(nodeId) {
 		if (firstChapter.verse) {
 			that.scrollTo(firstChapter.nodeId);
 		}
-		that.currentNodeId = firstChapter.nodeId;
+		that.currentNodeId = "top" + firstChapter.nodeId;
 		document.body.dispatchEvent(new CustomEvent(BIBLE.CHG_HEADING, { detail: { reference: firstChapter }}));
 		that.checkScrollID = window.setTimeout(onScrollHandler, CODEX_VIEW.SCROLL_TIMEOUT);	// should be last thing to do
 		
@@ -51,72 +51,73 @@ CodexView.prototype.showView = function(nodeId) {
 			if (verse) {
 				var rect = verse.getBoundingClientRect();
 				console.log("RECT TOP " + rect.top + "  window.scrollY " + window.scrollY);
-				//TweenMax.killTweensOf(window);
+				TweenMax.killTweensOf(window);
 				TweenMax.to(window, 0.7, {scrollTo: { y: rect.top + window.scrollY - that.headerHeight}});
-				//TweenMax.set(window, {scrollTo: { y: rect.top + window.scrollY - this.headerHeight}});
-				//window.scrollTo(0, rect.top + window.scrollY - that.headerHeight);
-			//} else {
-			//	var ref = Reference(nodeId);
-			//	that.showChapters([ref], true, function() {
-			//		verse = document.getElementById(nodeId);
-			//		if (verse) {
-			//			rect = verse.getBoundingClientRect();
-			//			TweenMax.to(window, 0.7, {scrollTo: { y: rect.top + window.scrollY - that.headerHeight}});
-			//		}
-			//	});
 			}	
 		});
 	});
 	function onScrollHandler(event) {
-		//console.log('windowHeight=', window.innerHeight, '  scrollHeight=', document.body.scrollHeight, '  scrollY=', window.scrollY);
-		//console.log('left', (document.body.scrollHeight - window.scrollY));
-		if (window.scrollY <= window.innerHeight && ! that.userIsScrolling) {
-			var firstNode = that.viewport.firstChild;
-			var firstChapter = new Reference(firstNode.id.substr(3));
-			var beforeChapter = that.tableContents.rowId(firstChapter) - 1;
-			if (beforeChapter) {
-				that.showChapters([beforeChapter], false, function() {
-					that.checkChapterQueueSize('bottom');
-					onScrollLastStep();
-				});
-			} else {
-				onScrollLastStep();
+		var currNode = identifyCurrentChapter();//expensive solution
+		//console.log("currNode top: " + currNode.top + "  mid: " + currNode.middle + "  bot: " + currNode.bottom);
+		if (currNode) {
+			if (currNode.middle !== that.currentNodeId) {
+				that.currentNodeId = currNode.middle;
+				var currRef = new Reference(currNode.middle.substr(3));
+				document.body.dispatchEvent(new CustomEvent(BIBLE.CHG_HEADING, { detail: { reference: currRef }}));
 			}
-		} else if (document.body.scrollHeight - window.scrollY <= 2 * window.innerHeight) {
-			var lastNode = that.viewport.lastChild;
-			var lastChapter = new Reference(lastNode.id.substr(3));
-			var nextChapter = that.tableContents.rowId(lastChapter) + 1;
-			if (nextChapter) {
-				that.showChapters([nextChapter], true, function() {
-					that.checkChapterQueueSize('top');
-					onScrollLastStep();
-				});
-			} else {
-				onScrollLastStep();
+			var firstChildId = that.viewport.firstChild.id;
+			var lastChildId = that.viewport.lastChild.id;
+			if (currNode.bottom === lastChildId || lastChildId.substr(7,1) === "0") {
+				var lastChapter = new Reference(lastChildId.substr(3));
+				var rowId = that.tableContents.rowId(lastChapter);
+				if (rowId) {
+					that.showChapters([rowId + 1], true, function() {
+						onScrollLastStep();
+					});					
+				} else onScrollLastStep();
 			}
-		} else {
-			onScrollLastStep();
-		}
+			else if (currNode.top === firstChildId || firstChildId.substr(7,1) === "0") {
+				var firstChapter = new Reference(firstChildId.substr(3));
+				var rowId = that.tableContents.rowId(firstChapter);
+				if (rowId) {
+					that.showChapters([rowId - 1], false, function() {
+						onScrollLastStep();
+					});
+				} else onScrollLastStep();
+			} else onScrollLastStep();
+		} else onScrollLastStep();
 	}
 	function onScrollLastStep() {
-		var ref = identifyCurrentChapter();//expensive solution
-		if (ref && ref.nodeId !== that.currentNodeId) {
-			that.currentNodeId = ref.nodeId;
-			document.body.dispatchEvent(new CustomEvent(BIBLE.CHG_HEADING, { detail: { reference: ref }}));
-		}
 		that.userIsScrolling = false;
 		that.checkScrollID = window.setTimeout(onScrollHandler, CODEX_VIEW.SCROLL_TIMEOUT); // should be last thing to do
 	}
 	function identifyCurrentChapter() {
-		var half = window.innerHeight / 2;
-		for (var i=that.viewport.children.length -1; i>=0; i--) {
-			var node = that.viewport.children[i];
-			var top = node.getBoundingClientRect().top;
-			if (top < half) {
-				return(new Reference(node.id.substr(3)));
+		var result = {top: null, middle: null, bottom: null};
+		var windowTop = that.headerHeight;
+		var windowMid = window.innerHeight / 2;
+		var windowBot = window.innerHeight;
+		var searchLimit = 0;
+		var index = that.viewport.children.length -1;
+		var searchLimit = 0;
+		while(index >= searchLimit) {
+			var node = that.viewport.children[index];
+			var rect = node.getBoundingClientRect();
+			if (result.bottom === null && rect.top < windowBot) {
+				result.bottom = node.id;
 			}
+			if (result.middle === null && rect.top < windowMid) {
+				result.middle = node.id;
+			}
+			if (rect.bottom > windowTop) {
+				result.top = node.id;
+			}
+			if (result.top && result.middle && searchLimit === 0) {
+				// search only three more chapters once top and middle are known
+				searchLimit = Math.max(index - 3, 0);
+			}
+			index--;
 		}
-		return(null);
+		return(result);
 	}
 };
 CodexView.prototype.showChapters = function(chapters, append, callback) {
@@ -150,6 +151,7 @@ CodexView.prototype.showChapters = function(chapters, append, callback) {
 * but tests showed that it is possible to have the entire Bible in one scroll
 * without a problem.  GNG 12/29/2015, ergo deprecated by setting MAX at 100000.
 */
+/*
 CodexView.prototype.checkChapterQueueSize = function(whichEnd) {
 	if (this.viewport.children.length > CODEX_VIEW.MAX) {
 		switch(whichEnd) {
@@ -169,6 +171,7 @@ CodexView.prototype.checkChapterQueueSize = function(whichEnd) {
 		console.log('discarded chapter ', discard.id.substr(3), 'at', whichEnd);
 	}
 };
+*/
 CodexView.prototype.scrollTo = function(nodeId) {
 	console.log('scrollTo', nodeId);
 	var verse = document.getElementById(nodeId);
