3be6ca3ecc625ac3bf0db8d89c26eeb0200e2734
Gary N Griswold <gary@shortsands.com>
2018-05-09 16:31:38 -0400

    Getting some done in TestApp.

--- TestApp/TestAppIOS/TestAppIOS/ViewController.swift
+++ TestApp/TestAppIOS/TestAppIOS/ViewController.swift
@@ -5,21 +5,47 @@
 //  Created by Gary Griswold on 5/9/18.
 //  Copyright © 2018 ShortSands. All rights reserved.
 //
-
+// https://developer.apple.com/documentation/webkit/wkwebview
+// script injection example
+// https://gist.github.com/morhekil/d1ff977e40b63cdd6379a8dac81e506c
+// call with return example
+// http://igomobile.de/2017/03/06/wkwebview-return-a-value-from-native-code-to-javascript/
+//
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler {
 
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let configuration = WKWebViewConfiguration()
+        configuration.userContentController.add(self, name: "callNative")
+        webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.uiDelegate = self
+        view = webView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let bundle = Bundle.main
+        print("path \(bundle.bundlePath)")
+        let path = bundle.path(forResource: "index", ofType: ".html")
+        let url = URL(fileURLWithPath: path!)
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
+        //let myURL = URL(string: "https://www.apple.com")
+        //let myRequest = URLRequest(url: myURL!)
+        //webView.load(myRequest)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /**
+     * var aMessage = {'command':'hello', data:[5,6,7,8,9]}
+     * window.webkit.messageHandlers.callNative.postMessage(aMessage)
+    */
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print("got message: \(message.body)")
     }
-
-
 }
 
---
+++ TestApp/www/test.js
@@ -0,0 +1,7 @@
+
+function displayLocale() {
+	console.log("start JS");
+	var element = document.getElementById("locale");
+	element.innerHTML = "en_US";
+	console.log("fin JS");
+}
