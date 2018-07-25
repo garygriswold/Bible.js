cfcc8ba09e07da08750dc4d1511d01675aebb511
Gary N Griswold <gary@shortsands.com>
2018-07-24 23:29:45 -0400

    Add Edit button to top.

--- Plugins/Settings/Settings_iOS2/StaticCellsSwift/SettingsViewController.swift
+++ Plugins/Settings/Settings_iOS2/StaticCellsSwift/SettingsViewController.swift
@@ -18,15 +18,37 @@ class SettingsViewController: UIViewController {
  
         // create Table view
         self.tableView = UITableView(frame: UIScreen.main.bounds, style: UITableViewStyle.grouped)
-        self.tableView.dataSource = self.dataSource
-        self.tableView.delegate = self.delegate
         self.view = self.tableView
  
         // set the view title
         self.title = "Settings"
         
-        //self.tableView.setEditing(true, animated: false)
+        // set Top Bar items
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneHandler))
+        self.saveHandler(sender: nil)
         
         self.tableView.register(VersionCell.self, forCellReuseIdentifier: "currVersion")
+        
+        self.tableView.dataSource = self.dataSource
+        self.tableView.delegate = self.delegate
+    }
+    
+    @objc func editHandler(sender: UIBarButtonItem?) {
+        self.tableView.setEditing(true, animated: true)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self,
+                                                                 action: #selector(saveHandler))
+    }
+    
+    @objc func saveHandler(sender: UIBarButtonItem?) {
+        self.tableView.setEditing(false, animated: true)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self,
+                                                                 action: #selector(editHandler))
+    }
+    
+    @objc func doneHandler(sender: UIBarButtonItem?) {
+        print("Done button clicked")
     }
 }
+
+
+// why does auto rotation not work
