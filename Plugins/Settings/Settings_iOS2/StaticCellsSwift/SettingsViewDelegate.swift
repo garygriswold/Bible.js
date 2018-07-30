b169563cb09a9e4263302586e70130a6eb82abd5
Gary N Griswold <gary@shortsands.com>
2018-07-29 23:36:35 -0400

    Tried to fix search bar growth.

--- Plugins/Settings/Settings_iOS2/StaticCellsSwift/SettingsViewDelegate.swift
+++ Plugins/Settings/Settings_iOS2/StaticCellsSwift/SettingsViewDelegate.swift
@@ -15,7 +15,9 @@ class SettingsViewDelegate : NSObject, UITableViewDelegate {
         print("****** Deinit SettingsViewDelegate ******")
     }
     
-    //func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {}
+    //func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        //return UITableViewAutomaticDimension
+    //}
     
     //func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {}
     
--- Plugins/Settings/Settings_iOS2/StaticCellsSwift/VersionSearchCell.swift
+++ Plugins/Settings/Settings_iOS2/StaticCellsSwift/VersionSearchCell.swift
@@ -20,7 +20,7 @@ class VersionSearchCell : UITableViewCell {
         super.init(style: UITableViewCellStyle.default, reuseIdentifier: nil)
     }
     required init?(coder: NSCoder) {
-        self.searchBar = UISearchBar() // This won't work, because it is not attached to a controll
+        self.searchBar = UISearchBar() // This won't work, because it is not attached to a control
         super.init(coder: coder)
     }
     
@@ -29,10 +29,10 @@ class VersionSearchCell : UITableViewCell {
         
         self.searchBar.frame = self.bounds
         self.searchBar.placeholder = "Find Bibles"
-        self.searchBar.barTintColor = UIColor(red: 0.179, green: 0.617, blue: 0.785, alpha: 1.0) // #2E9EC9;
-        self.searchBar.searchBarStyle = UISearchBarStyle.prominent // can be (minimal or prominent)
-        //self.searchBar.showsCancelButton = false
-        //self.searchBar.setShowsCancelButton(false, animated: false)
+        //self.searchBar.barTintColor = UIColor(red: 0.179, green: 0.617, blue: 0.785, alpha: 1.0) // #2E9EC9;
+        self.searchBar.searchBarStyle = UISearchBarStyle.default // can be (defult or minimal or prominent)
+        self.searchBar.showsCancelButton = false
+        //self.searchBar.setShowsCancelButton(true, animated: true)
         //self.searchBar.showsBookmarkButton = true // if I want to implement bookmark
         //self.searchBar.showsSearchResultsButton = true // if I want way to see prior results
         self.addSubview(self.searchBar)
