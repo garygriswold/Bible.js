ac9795cf3e0da3ea40c28a032c73646155a23da9
Gary N Griswold <gary@shortsands.com>
2018-08-10 19:29:47 -0400

    Settings rewrote program to get language codes using SIL as a source.

--- Versions/Release.1.13/py/LanguageTable.py
+++ Versions/Release.1.13/py/LanguageTable.py
@@ -1,32 +1,76 @@
 #
 # This program generates SQL statements to create and populate the Language table
+# It reads SIL provided 639 tables to do this
 #
-import sys
+# iso-639-3.txt
+#	tab delimited, line 1 is heading
+#	0. iso
+#	1. iso2B
+#	2. iso2T
+#	3. iso1
+#	4. scope I=individual, M=metalanguage
+#	5. type L, E, C, H, A
+#	6. name
+#
+# iso-639-3-macrolanguages.txt
+#	tab delimited, line 1 is heading
+#	0. macro iso
+#	1. iso
+#	2. status A
+#
+
 import io
-import json
+
+macroMap = {}
+# Read in macrolanguage table, and create table of macro codes iso : macro
+input1 = io.open("metadata/iso-639-3/iso-639-3-macrolanguages.txt", mode="r", encoding="utf-8")
+for line in input1:
+    row = line.split("\t")
+    macroMap[row[1]] = row[0]
+input1.close()
+
+iso1Map = {}
+isoTable = []
+# Read in 639 table, lookup in macro table, add macro to table and create a map for ios1
+input2 = io.open("metadata/iso-639-3/iso-639-3.txt", mode="r", encoding="utf-8")
+for line in input2:
+	row = line.split("\t")
+	iso = row[0]
+	if (iso != "Id"):
+		iso1 = row[3]
+		if (len(iso1) > 0):
+			iso1Map[iso] = iso1
+		name = row[6]
+		macro = macroMap.get(iso, "")
+		isoTable.append([iso, iso1, macro, name])
+input2.close()
 
 out = io.open("sql/language.sql", mode="w", encoding="utf-8")
 
 out.write(u"DROP TABLE IF EXISTS Language;\n")
 out.write(u"CREATE TABLE Language (\n")
 out.write(u"  iso TEXT NOT NULL PRIMARY KEY,\n")
-out.write(u"  name TEXT NOT NULL,\n")
 out.write(u"  iso1 TEXT NULL,\n")
-out.write(u"  rightToLeft CHECK (rightToLeft IN('T','F')) default('F'),\n")
-out.write(u"  english TEXT NULL);\n")
-prefix = "REPLACE INTO Language (iso, name, iso1, english) VALUES"
+out.write(u"  macro TEXT NULL,\n")
+out.write(u"  name TEXT NOT NULL);\n")
+prefix = "REPLACE INTO Language (iso, iso1, macro, name) VALUES"
 
-data = sys.stdin.read()
-print "Counted", len(data), "chars."
-langs = json.loads(data)
-
-for lang in langs:
-	iso = lang['language_iso']
-	name = lang['language_name'].replace("\\", "").replace("'", "''")
-	iso1 = lang['language_iso_1']
-	english = lang['english_name'].replace("\\", "").replace("'", "''")
-	iso1 = 'null' if (iso1 is None or len(iso1) == 0) else  "'" + iso1 + "'"
-	english = 'null' if (english is None or english == name) else "'" + english + "'"
-	out.write("%s ('%s', '%s', %s, %s);\n" % (prefix, iso, name, iso1, english))
+# Output table, and looking ios1 for any language with a macro language
+for lang in isoTable:
+	iso = lang[0]
+	iso1 = lang[1]
+	macro = lang[2]
+	name = lang[3].replace("\\", "").replace("'", "''")
+	if (len(iso1) == 0):
+		iso1 = iso1Map.get(macro, "")
+	iso = "'" + iso + "'"
+	iso1 = "'" + iso1 + "'" if (len(iso1) > 0) else 'null'
+	macro = "'" + macro + "'" if (len(macro) > 0) else 'null'
+	name = "'" + name + "'"
+	out.write("%s (%s, %s, %s, %s);\n" % (prefix, iso, iso1, macro, name))
 
 out.close()
+
+
+
+
