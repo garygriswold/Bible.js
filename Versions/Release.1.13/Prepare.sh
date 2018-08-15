f44c8cc2fe3bcf542fd930510028f2dacd1bc541
Gary N Griswold <gary@shortsands.com>
2018-08-15 19:34:54 -0400

    Settings add ISO3Priority to Language table.

--- Versions/Release.1.13/Prepare.sh
+++ Versions/Release.1.13/Prepare.sh
@@ -18,6 +18,26 @@ sqlite Versions.db < sql/iso3Priority.sql
 sqlite Versions.db < sql/copied_owner.sql
 sqlite Versions.db < sql/bible.sql
 
+# Merge ISO3Priority into Language
+sqlite Versions.db <<END_SQL1
+DROP TABLE IF EXISTS LanguageTemp;
+ALTER TABLE Language RENAME TO LanguageTemp;
+
+CREATE TABLE Language (
+  iso3 TEXT NOT NULL PRIMARY KEY,
+  iso1 TEXT NULL,
+  macro TEXT NULL,
+  name TEXT NOT NULL,
+  country TEXT NULL REFERENCES Country(code),
+  pop REAL NULL);
+
+INSERT INTO Language
+select l.iso3, l.iso1, l.macro, l.name, p.country, p.pop 
+FROM LanguageTemp l LEFT OUTER JOIN ISO3Priority p 
+ON p.iso3=l.iso3;
+END_SQL1
+
+# Remove Languages and Bibles that are not used.
 sqlite Versions.db <<END_SQL
 select count(*) AS Language_Count from Language;
 select count(*) AS Bibles_Count from Bible;
