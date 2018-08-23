249b6c9887d5d2d28836de2ac6453c953dba4714
Gary N Griswold <gary@shortsands.com>
2018-08-23 15:07:39 -0400

    Versions Add DBPDownloadInfo.

--- Versions/Release.1.13/py/DBPDownloadInfo.py
+++ Versions/Release.1.13/py/DBPDownloadInfo.py
@@ -4,32 +4,23 @@
 #
 import boto3
 import io
-import os
+import os 
 
-media = "text/ENGESV"
-mediaLen = len(media)
-filename1 = "about.html"
-filename2 = "info.json"
-target = "/Users/garygriswold/ShortSands/DBL/FCBH_info"
+searchFile = "info.json"
+target = "/Users/garygriswold/ShortSands/DBL/FCBH_info/"
 
 session = boto3.Session(profile_name='FCBH_BibleApp')
 client = session.client('s3')
 
-input = io.open("metadata/FCBH/dbp_dev.txt", mode="r", encoding="utf-8")
+input = io.open("metadata/FCBH/dbp_prod.txt", mode="r", encoding="utf-8")
 for line in input:
-	if line[0:mediaLen] == media:
-		line = line.strip()
-		row = line.split("/")
-		last = row[-1]
-		if last == filename1 or last == filename2:
-			directory = target + "/" + row[1]
-			if not os.path.exists(directory):
-				os.makedirs(directory)
-			path = directory + "/" + last
-			try:
-				client.download_file('dbp-prod', line, path)
-				print "Sucessfully downloaded", path
-			except:
-				print "Failed ", line
+	line = line.strip()
+	if line.endswith(searchFile):
+		filename = target + line.replace("/", "|")
+		try:
+			client.download_file('dbp-prod', line, filename)
+			print "Done ", line
+		except:
+			print "Error Failed ", line
 
 input.close()
