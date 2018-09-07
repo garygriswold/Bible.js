201d7b3bfc8f6be700710ce75659214b65666e30
Gary N Griswold <gary@shortsands.com>
2018-09-07 13:15:57 -0400

    Versions checking audio output from prior release.

---
+++ Versions/output/AudioVersionTable.sql
@@ -0,0 +1,12 @@
+INSERT INTO AudioVersion VALUES ('ERV-BUL', 'BLG', 'AMB');
+INSERT INTO AudioVersion VALUES ('ERV-CMN', 'CHN', 'UNV');
+INSERT INTO AudioVersion VALUES ('ERV-ENG', 'ENG', 'ESV');
+INSERT INTO AudioVersion VALUES ('KJVPD', 'ENG', 'KJV');
+INSERT INTO AudioVersion VALUES ('WEB', 'ENG', 'WEB');
+INSERT INTO AudioVersion VALUES ('ERV-HUN', 'HUN', 'HBS');
+INSERT INTO AudioVersion VALUES ('ERV-IND', 'INZ', 'SHL');
+INSERT INTO AudioVersion VALUES ('ERV-POR', 'POR', 'ARA');
+INSERT INTO AudioVersion VALUES ('ERV-RUS', 'RUS', 'S76');
+INSERT INTO AudioVersion VALUES ('ERV-SPA', 'SPN', 'R95');
+INSERT INTO AudioVersion VALUES ('ERV-UKR', 'UKR', 'O95');
+INSERT INTO AudioVersion VALUES ('ERV-URD', 'URD', 'PAK');
