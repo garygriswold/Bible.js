871f6a73703b5a9d972f892328fc554f397b6129
Gary N Griswold <gary@shortsands.com>
2018-09-28 17:22:27 -0400

    Settings store review written.

--- Plugins/Settings/Settings_ios/Settings/StoreReviewController.swift
+++ Plugins/Settings/Settings_ios/Settings/StoreReviewController.swift
@@ -7,7 +7,11 @@
 //
 
 import StoreKit
-
+/**
+ * This class accumulates the total seconds use of the App, and everytime
+ * that use reaches a threshold (at this time 30 min), it attempts to present
+ * the App review dialog.
+ */
 class StoreReviewController {
     
     public static let shared = StoreReviewController()
@@ -38,25 +42,33 @@ class StoreReviewController {
                            object: nil)
     }
     
+    deinit {
+        print("****** deinit StoreReviewController ******")
+    }
+    
     @objc func startTimer(note: Notification) {
-        print("**** applicationDidFinishLaunching ****")
+        print("**** applicationDidLaunch or EnteredForeground ****")
         self.appStart = CFAbsoluteTimeGetCurrent()
         let settings = UserDefaults.standard
         let seconds = settings.double(forKey: StoreReviewController.APP_USE_SEC)
-        let secToNext = seconds.truncatingRemainder(dividingBy: StoreReviewController.REVIEW_INTERVAL_SEC)
+        let remainder = seconds.truncatingRemainder(dividingBy: StoreReviewController.REVIEW_INTERVAL_SEC)
+        let secToNext = StoreReviewController.REVIEW_INTERVAL_SEC - remainder
+        print("seconds \(seconds)  remainer \(remainder)  secToNext \(secToNext)")
         self.timer = Timer.scheduledTimer(timeInterval: secToNext, target: self, selector:
             #selector(self.showReviewDialog), userInfo: nil, repeats: false)
     }
     @objc func showReviewDialog() {
+        print("StoreReviewController called")
         SKStoreReviewController.requestReview()
     }
     @objc func completeTimer(note: Notification) {
-        print("**** applicationDidEnterBackground ****")
+        print("**** applicationEnteredBackground or Terminated ****")
         let settings = UserDefaults.standard
         let currentSec = CFAbsoluteTimeGetCurrent() - self.appStart
         var seconds = settings.double(forKey: StoreReviewController.APP_USE_SEC)
         seconds += currentSec
         settings.set(seconds, forKey: StoreReviewController.APP_USE_SEC)
+        print("curr use \(currentSec) new seconds \(seconds)")
         self.timer?.invalidate()
     }
 }
