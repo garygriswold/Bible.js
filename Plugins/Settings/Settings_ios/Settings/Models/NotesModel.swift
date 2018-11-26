9ca83c7fb4827bd4ea5c0b85fb17cf1b40e7828c
Gary N Griswold <gary@shortsands.com>
2018-11-26 00:54:38 -0500

    Settings added encoding of range, and install of effects on select.

--- Plugins/Settings/Settings_ios/Settings/Models/NotesModel.swift
+++ Plugins/Settings/Settings_ios/Settings/Models/NotesModel.swift
@@ -5,41 +5,45 @@
 //  Created by Gary Griswold on 11/16/18.
 //  Copyright © 2018 ShortSands. All rights reserved.
 //
+import Foundation
 
 struct Note {
     
     var bookId: String
     var chapter: Int            // 0 means any chapter in book
-    var verse: Int              // 0 means any verse in chapter
+    var datetime: TimeInterval  //
+    var verseStart: Int         // 0 means any verse in chapter
+    var verseEnd: Int           // 0 means any verse in chapter
     var bibleId: String         // 0 means any version
     var bookmark: Bool
     var highlightColor: String? // color name, presence indicates highlight
-    var startChar: Int?         // optional for highlight
-    var endChar: Int?           // optional for highlight
+    var selection: String
     var note: String?
     
     init(bookId: String, chapter: Int, verse: Int, bibleId: String, bookmark: Bool,
-         highlightColor: String?, startChar: Int?, endChar: Int?, note: String?) {
+         highlightColor: String?, selection: String, note: String?) {
         self.bookId = bookId
         self.chapter = chapter
-        self.verse = verse
+        self.datetime = Date().timeIntervalSince1970
+        self.verseStart = verse
+        self.verseEnd = verse
         self.bibleId = "0"
         self.bookmark = true
         self.highlightColor = highlightColor
-        self.startChar = startChar
-        self.endChar = endChar
+        self.selection = selection
         self.note = note
     }
     
     init(bookId: String, chapter: Int, verse: Int) {
         self.bookId = bookId
         self.chapter = chapter
-        self.verse = verse
+        self.datetime = Date().timeIntervalSince1970
+        self.verseStart = verse
+        self.verseEnd = verse
         self.bibleId = "0"
         self.bookmark = true
         self.highlightColor = nil
-        self.startChar = nil
-        self.endChar = nil
+        self.selection = "" // OK?
         self.note = nil
     }
     
@@ -62,10 +66,42 @@ struct Note {
         + "  }\n"
         + "}\n"
     
-    static let encodeRange = "function encodeRange(range) {"
-    
-    //    return string
-    //}
+    static let encodeRange = "function encodeRange(range) {\n"
+        + "  var startNode = range.startContainer;\n"
+        + "  var endNode = range.endContainer;\n"
+        + "  var startId = identElement(startNode);\n"
+        + "  var endId = identElement(endNode);\n"
+        + "  var startChars = range.startOffset + adjustStartChar(startNode);\n"
+        + "  var endChars = range.endOffset + adjustStartChar(endNode);\n"
+        + "  return startId + ':' + startChars + '/' + endId + ':' + endChars;\n"
+        + "}\n"
+        + "function identElement(node) {\n"
+        + "  var element = node.parentNode;\n"
+        + "  var clas = element.className;\n"
+        + "  var list = document.getElementsByClassName(clas);\n"
+        + "  for (var i=0; i<list.length; i++) {\n"
+        + "    var item = list[i];\n"
+        + "    if (item === element) {\n"
+        + "      return(clas + ':' + i);\n"
+        + "    }\n"
+        + "  }\n"
+        + "  return null;\n"
+        + "}\n"
+        + "function adjustStartChar(node) {\n"
+        + "  var countChars = 0;\n"
+        + "  var element = node.parentNode;\n"
+        + "  if (element.hasChildNodes()) {\n"
+        + "    for (var i=0; i<element.childNodes.length; i++) {"
+        + "      var child = element.childNodes[i];\n"
+        + "      if (child !== node) {\n"
+        + "        countChars += child.textContent.length;\n"
+        + "      } else {\n"
+        + "        return countChars;\n"
+        + "      }\n"
+        + "    }\n"
+        + "  }\n"
+        + "  return countChars;\n"
+        + "}\n"
     
     static let decodeRange = "function decodeRange(string) {"
     
@@ -75,7 +111,7 @@ struct Note {
 
 struct NotesModel {
     
-    
+ /*
     static func unitTest() {
         //1. init a note object with all none null values
         let note1 = Note(bookId: "JHN", chapter: 3, verse: 3, bibleId: "ENGWEB", bookmark: true,
@@ -99,5 +135,5 @@ struct NotesModel {
         let notes1 = SettingsDB.shared.getNotes(bookId: "JHN", chapter: 3, bibleId: "ENGWEB")
         //10. view results with print
         print("NOTE \(notes1)")
-    }
+    }*/
 }
--- Plugins/Settings/Settings_ios/Settings/ViewControllers/WKWebViewExtension.swift
+++ Plugins/Settings/Settings_ios/Settings/ViewControllers/WKWebViewExtension.swift
@@ -71,23 +71,36 @@ extension WKWebView {
     
     @objc func bookmarkHandler(sender: UIMenuController) {
         let query = "var select = window.getSelection();\n"
-            + "installEffect(select.getRangeAt(0), 'book');\n"
+            + "var range = select.getRangeAt(0);\n"
+            + "installEffect(range, 'book');\n"
+            + "window.getSelection().removeAllRanges();\n"
+            + "encodeRange(range);\n"
             + Note.installEffect
+            + Note.encodeRange
         self.evaluateJavaScript(query, completionHandler: { data, error in
             if let err = error {
                 print("ERROR: insertBookmarkIcon \(err)")
             }
+            if let range = data {
+                print("RANGE found \(range)")
+            }
         })
-        //self.selectionVerseStart()
     }
     
     @objc func noteHandler(sender: UIMenuItem) {
         let query = "var select = window.getSelection();\n"
-            + "installEffect(select.getRangeAt(0), 'note');\n"
+            + "var range = select.getRangeAt(0);\n"
+            + "installEffect(range, 'note');\n"
+            + "window.getSelection().removeAllRanges();\n"
+            + "encodeRange(range);\n"
             + Note.installEffect
+            + Note.encodeRange
         self.evaluateJavaScript(query, completionHandler: { data, error in
             if let err = error {
                 print("ERROR: insertNoteIcon \(err)")
+            }
+            if let range = data {
+                print("RANGE found \(range)")
             }
         })
     }
@@ -117,125 +130,28 @@ extension WKWebView {
             }
         })
     }
- /*
-    private func selectionVerseStart() {
-        let query = "var clas = '';\n"
-            + "var select = window.getSelection();\n"
-            + "if (select != null) {\n"
-            + "  var range = select.getRangeAt(0);\n"
-            + "  var startNode = range.startContainer;\n"
-            + "  if (startNode.nodeType != 1) {\n"
-            + "    startNode = startNode.parentElement;\n"
-            + "  }\n"
-            + "  clas = startNode.className;\n"
-            + "}\n"
-            + "clas;\n"
-        print(query)
-        self.evaluateJavaScript(query, completionHandler: { data, error in
-            if let err = error {
-                print("ERROR: selectionVerseStart \(err)")
-            }
-            if let resp = data as? String {
-                print("jsCallback has a response \(resp)")
-                let resp2 = resp.replacingOccurrences(of: "-", with: "_")
-                let parts = resp2.split(separator: "_")
-                let verse = String(parts.last!)
-                print(verse)
-                self.insertBookmark(verse: verse)
-                let ref = HistoryModel.shared.current()
-                let bookmark = Note(bookId: ref.bookId, chapter: ref.chapter, verse: Int(verse) ?? 0)
-                SettingsDB.shared.storeNote(note: bookmark)
-            } else {
-                print("ERROR: selectionVerseStart returns non-string \(String(describing: data))")
-            }
-        })
-    }
- */
- /*
-    private func findSelection(selectionUse: SelectionUse) {
-        let query = "var select = window.getSelection();\n"
-            + "var range = select.getRangeAt(0);\n"
-            + "var startNode = range.startContainer.parentElement;\n"
-            + "var endNode = range.endContainer.parentElement;\n"
-            + "var topNode = range.commonAncestorContainer;\n"
-            + "if (topNode.nodeType === 3) {\n"
-            + "  topNode = topNode.parentElement;\n"
-            + "}\n"
-            + "var elementList = [];\n"
-            + "var isInSelection = false;\n"
-            + "depthFirst(topNode);\n"
-            + "var identList = [range.startOffset, range.endOffset];\n"
-            + "for (var i=0; i<elementList.length; i++) {\n"
-            + "  identList.push(findNode(elementList[i]));\n"
-            + "}\n"
-            + "window.getSelection().removeAllRanges();\n"
-            + "identList.join('/');\n" // return to evaluateJavascript
-            + "function depthFirst(node) {\n"
-            + "  if (node === startNode) {\n"
-            + "    isInSelection = true;\n"
-            + "  }\n"
-            + "  if (isInSelection && node.children.length == 0) {\n"
-            + "    elementList.push(node);\n"
-            + "  }\n"
-            + "  if (node === endNode) {\n"
-            + "    isInSelection = false;\n"
-            + "  }\n"
-            + "  for (var i=0; i<node.children.length; i++) {\n"
-            + "    depthFirst(node.children[i]);\n"
-            + "  }\n"
-            + "}\n"
-            + "function findNode(node) {\n"
-            + "  var clas = node.className;\n"
-            + "  var list = document.getElementsByClassName(clas);\n"
-            + "  for (var i=0; i<list.length; i++) {\n"
-            + "    var item = list[i];\n"
-            + "    if (item == node) {\n"
-            + "      return(clas + ':' + i);\n"
-            + "    }\n"
-            + "  }\n"
-            + "  return null;\n"
-            + "}\n"
-        self.evaluateJavaScript(query, completionHandler: { data, error in
-            if let resp = data as? String {
-                print(resp)
-                _ = Selection.factory(selection: resp)
-                switch selectionUse {
-                case .highlight:
-                    let colorPicker = ColorPicker(webView: self)
-                    self.addSubview(colorPicker)
-                case .bookmark:
-                    _ = 2
-                case .note:
-                    _ = 3
-                    // This one primarily needs a starting point
-                case .compare:
-                    _ = 4
-                    // This one can only be whole verses.  It can be multiple,
-                    // but it cannot be part of a verse.
-                }
-            }
-            if let err = error {
-                print("ERROR: findSelection \(err)")
-            }
-        })
-    }
- */
+ 
     @objc public func colorTouchHandler(sender: UITapGestureRecognizer) {
         sender.view?.superview?.removeFromSuperview()
         if let color = sender.view?.backgroundColor {
             let hexColor = ColorPicker.toHEX(color: color)
             print("tapped colored dot \(hexColor)")
             let query = "var select = window.getSelection();\n"
-                + "installEffect(select.getRangeAt(0), 'lite', '\(hexColor)');\n"
+                + "var range = select.getRangeAt(0);\n"
+                + "installEffect(range, 'lite', '\(hexColor)');\n"
+                + "window.getSelection().removeAllRanges();\n"
+                + "encodeRange(range);\n"
                 + Note.installEffect
+                + Note.encodeRange
             self.evaluateJavaScript(query, completionHandler: { data, error in
                 if let err = error {
                     print("ERROR: insertHighlight \(err)")
                 }
+                if let range = data {
+                    print("RANGE found \(range)")
+                }
             })
         }
-        // have selection respond to user selection by inserting selection.
-        // Use History to get reference
     }
 
     func addNotes(reference: Reference) {
