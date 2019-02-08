da6ec74560779c4d0f0d244eb0bbc9cf8ba0387e
Gary N Griswold <gary@shortsands.com>
2019-02-08 00:44:18 -0500

    Start Prepare.sh

---
+++ Versions/Release.2.0.x/Prepare.sh
@@ -0,0 +1,31 @@
+
+############################ Upload Bibles ############################
+
+python py/UploadBibleText.py ARBVDPD.db
+python py/UploadBibleText.py ERV-ARB.db
+python py/UploadBibleText.py ERV-AWA.db
+python py/UploadBibleText.py ERV-BEN.db
+python py/UploadBibleText.py ERV-BUL.db
+python py/UploadBibleText.py ERV-CMN.db
+python py/UploadBibleText.py ERV-ENG.db
+python py/UploadBibleText.py ERV-HIN.db
+python py/UploadBibleText.py ERV-HRV.db
+python py/UploadBibleText.py ERV-HUN.db
+python py/UploadBibleText.py ERV-IND.db
+python py/UploadBibleText.py ERV-KAN.db
+python py/UploadBibleText.py ERV-MAR.db
+python py/UploadBibleText.py ERV-NEP.db
+python py/UploadBibleText.py ERV-ORI.db
+python py/UploadBibleText.py ERV-PAN.db
+python py/UploadBibleText.py ERV-POR.db
+python py/UploadBibleText.py ERV-RUS.db
+python py/UploadBibleText.py ERV-SPA.db
+python py/UploadBibleText.py ERV-SRP.db
+python py/UploadBibleText.py ERV-TAM.db
+python py/UploadBibleText.py ERV-THA.db
+python py/UploadBibleText.py ERV-UKR.db
+python py/UploadBibleText.py ERV-URD.db
+python py/UploadBibleText.py ERV-VIE.db
+python py/UploadBibleText.py KJVPD.db
+python py/UploadBibleText.py NMV.db
+python py/UploadBibleText.py WEB.db
