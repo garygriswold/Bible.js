2978ff05918840c4f92336450f95a4957eafb0e9
Gary N Griswold <gary@shortsands.com>
2019-02-11 00:06:08 -0500

    Added Support tables to Versions.db in Release 2.0

--- Versions/Release.2.0.x/Prepare.sh
+++ Versions/Release.2.0.x/Prepare.sh
@@ -35,3 +35,100 @@ python py/UploadBibleText.py WEB.db
 # python py/ListBucket.py > west_2.out # to List uploaded Bibles
 # diff dbp_prod.out west_2.out
 
+############################ Support Tables ############################
+
+# Create and Load Credentials Table
+sqlite Versions.db < Credentials.sql
+
+# Create and Load Owner Table
+sqlite Versions.db < sql/Owners.sql
+
+#Create and Lood Region Table
+sqlite Versions.db << END_SQL1
+CREATE TABLE Region (
+countryCode TEXT NOT NULL PRIMARY KEY,
+continentCode TEXT NOT NULL CHECK (continentCode IN('AF','EU','AS','NA','SA','OC','AN')),
+geoschemeCode TEXT NOT NULL CHECK (geoschemeCode IN(
+		'AF-EAS','AF-MID','AF-NOR','AF-SOU','AF-WES',
+		'SA-CAR','SA-CEN','SA-SOU','NA-NOR',
+		'AS-CEN','AS-EAS','AS-SOU','AS-SEA','AS-WES',
+		'EU-EAS','EU-NOR','EU-SOU','EU-WES',
+		'OC-AUS','OC-MEL','OC-MIC','OC-POL','AN-ANT')),
+awsRegion TEXT NOT NULL REFERENCES AWSRegion(awsRegion),
+countryName TEXT NOT NULL	
+);
+CREATE INDEX Region_awsRegion_index ON Region(awsRegion);
+.separator '|'
+.import data/Region.txt Region
+END_SQL1
+# Continent Code
+# AF|Africa
+# EU|Europe
+# AS|Asia
+# NA|North America
+# SA|South America
+# OC|Oceana
+# AN|Antartica
+
+# United Nations geoschema
+# AF-EAS|Eastern Africa
+# AF-MID|Middle Africa
+# AF-NOR|Northern Africa
+# AF-SOU|Southern Africa
+# AF-WES|Western Africa
+# SA-CAR|Caribbean
+# SA-CEN|Central America
+# SA-SOU|South America
+# NA-NOR|North America
+# AS-CEN|Central Asia
+# AS-EAS|Eastern Asia
+# AS-SOU|Southern Asia
+# AS-SEA|South-Eastern Asia
+# AS-WES|Western Asia
+# EU-EAS|Eastern Europe
+# EU-NOR|Northern Europe
+# EU-SOU|Southern Europe
+# EU-WES|Western Europe
+# OC-AUS|Australia and New Zealeand
+# OC-MEL|Melanesia
+# OC-MIC|Micronesia
+# OC-POL|Polynesia
+
+# Rules for bucket assignment:
+# 1) AF -> eu-west-1
+# 2) EU -> eu-west-1
+# 3) OC -> ap-southeast-2
+# 4) AN -> ap-southeast-2
+# 5) NA -> us-east-1
+# 6) SA -> us-east-1
+# 7) AS-WES -> eu-west-1
+# 8) AS-SOU -> ap-southeast-1
+# 9) AS-SEA -> ap-southeast-1
+# 10) AS-CEN -> ap-northeast-1
+# 11) AS-EAS -> ap-northeast-1
+
+# Create and Load LanguageTemp and ISO3Priority
+python py/LanguageTable.py
+python py/ISO3PriorityTable.py
+
+sqlite Versions.db < sql/language.sql
+sqlite Versions.db < sql/iso3Priority.sql
+
+# Create and Load Language
+sqlite Versions.db <<END_SQL2
+DROP TABLE IF EXISTS Language;
+CREATE TABLE Language (
+  iso3 TEXT NOT NULL PRIMARY KEY,
+  iso1 TEXT NULL,
+  macro TEXT NULL,
+  name TEXT NOT NULL,
+  country TEXT NULL REFERENCES Country(code),
+  pop REAL NULL);
+
+INSERT INTO Language
+select l.iso3, l.iso1, l.macro, l.name, p.country, p.pop 
+FROM LanguageTemp l LEFT OUTER JOIN ISO3Priority p 
+ON p.iso3=l.iso3;
+END_SQL2
+
+# must drop LanguageTemp and ISO3Priority
