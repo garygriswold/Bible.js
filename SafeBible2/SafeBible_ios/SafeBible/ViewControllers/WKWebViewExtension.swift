fe92149f1ae3a3c1d3c9ebcae0977eea9ba826a8
Gary N Griswold <gary@shortsands.com>
2019-02-22 23:39:30 -0500

    Fix bug in share to remove selection when done.

--- SafeBible2/SafeBible_ios/SafeBible/ViewControllers/WKWebViewExtension.swift
+++ SafeBible2/SafeBible_ios/SafeBible/ViewControllers/WKWebViewExtension.swift
@@ -82,7 +82,9 @@ extension WKWebView {
     }
     
     @objc func shareHandler(sender: UIMenuItem) {
-        let query = "window.getSelection().toString();"
+        let query = "var select = window.getSelection();\n"
+        + "select.removeAllRanges();\n"
+        + "select.toString();"
         self.evaluateJavaScript(query, completionHandler: { data, error in
             if let err = error {
                 print("ERROR: shareHandler \(err)")
