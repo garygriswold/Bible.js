6fd08f8b361912b0ed5a709a5223c611c5ab0af4
Gary N Griswold <gary@shortsands.com>
2019-03-20 11:20:03 -0400

    Remove commented code.

--- SafeBible2/SafeBible_ios/SafeBible/Models/NotesModel.swift
+++ SafeBible2/SafeBible_ios/SafeBible/Models/NotesModel.swift
@@ -243,29 +243,4 @@ struct Note {
 
 struct NotesModel {
     
- /*
-    static func unitTest() {
-        //1. init a note object with all none null values
-        let note1 = Note(bookId: "JHN", chapter: 3, verse: 3, bibleId: "ENGWEB", bookmark: true,
-                         highlightColor: "green", startChar: 12, endChar: 24,
-                         note: "It was a dark and stormy night")
-        //2. store using SettingsDB
-        SettingsDB.shared.storeNote(note: note1)
-        //3. inspect DB
-        //4. init a note object with mostly null objects
-        let note2 = Note(bookId: "JHN", chapter: 0, verse: 0, bibleId: "0", bookmark: false,
-                         highlightColor: nil, startChar: nil, endChar: nil,
-                         note: nil)
-        //5. store using SettingsDB
-        SettingsDB.shared.storeNote(note: note2)
-        //6. inspect DB
-        //7. queryDB for all
-        let notes = SettingsDB.shared.getNotes(bookId: "JHN", chapter: 0, bibleId: "0")
-        //8. view results with print
-        print("NOTES \(notes)")
-        //9. queryDB for one
-        let notes1 = SettingsDB.shared.getNotes(bookId: "JHN", chapter: 3, bibleId: "ENGWEB")
-        //10. view results with print
-        print("NOTE \(notes1)")
-    }*/
 }
