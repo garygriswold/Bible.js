effdcf9c13e536433c4859383423170ebe1ef16f
Gary N Griswold <gary@shortsands.com>
2019-04-13 00:09:36 -0400

    Removed obsolete search2

--- SafeBible2/SafeBible_ios/SafeBible/Models/ConcordanceModel.swift
+++ SafeBible2/SafeBible_ios/SafeBible/Models/ConcordanceModel.swift
@@ -185,75 +185,6 @@ struct ConcordanceModel {
         return updatedPositions
     }
 
-    func search2old(bible: Bible, words: [String]) -> [String] {
-        if words.count == 0 {
-            return [String]()
-        }
-        let refLists2 = BibleDB.shared.selectRefList2(bible: bible, words: words)
-        if refLists2.count != words.count {
-            return [String]()
-        }
-        let setList: [Set<String>] = refLists2.map { Set($0) }
-        //print(setList)
-        
-        var result = [String]()
-        let firstList = refLists2[0]
-        for reference in firstList {
-            if self.matchEachWord(setList: setList, reference: reference) {
-                result.append(reference)
-            }
-        }
-        return result
-    }
-    
-    private func matchEachWord(setList: [Set<String>], reference: String) -> Bool {
-        var wordRef = TempWordRef(reference: reference)
-        for set in setList {
-            if !set.contains(wordRef.reference) {
-                return false
-            }
-            wordRef.next()
-        }
-        return true
-    }
-//        function matchEachWord(mapList, reference) {
-//            var resultItem = [ reference ];
-//            for (var i=0; i<mapList.length; i++) {
-//                reference = matchWordWithLookahead(mapList[i], reference);
-//                if (reference == null) {
-//                    return(null);
-//                }
-//                resultItem.push(reference);
-//            }
-//            return(resultItem);
-//
-//}
-/*
-    private func matchWordWithLookahead(refSet: Set<String>, reference: TempWordRef) -> Bool {
-        for look in 0..<(self.wordsLookAhead + 2) {
-            let next = reference.reference(add: look)
-            if refSet.contains(next) {
-                return true
-            }
-        }
-        return false
-    }
- */
-//        function matchWordWithLookahead(mapRef, reference) {
-//            for (var look=1; look<=that.wordsLookAhead + 1; look++) {
-//                var next = nextPosition(reference, look);
-//                if (mapRef[next]) {
-//                    return(next);
-//                }
-//            }
-//            return(null);
-//        }
-//        function nextPosition(reference, position) {
-//            var parts = reference.split(';');
-//            var next = parseInt(parts[1]) + position;
-//            return(parts[0] + ';' + next.toString());
-//        }
-//    };
     /**
      * This method searches for all verses that contain all of the words entered.
      * It also ensures that the words are in the sequence entered in the search.
@@ -305,21 +236,6 @@ struct ConcordanceModel {
             }
         }
         return true
-    }
-    
-    //
-    // Shared?
-    //
-    private func findShortest(refLists: [[String]]) -> [String] {
-        var count = 100000
-        var best = 0
-        for index in 0..<refLists.count {
-            if refLists[index].count < count {
-                count = refLists[index].count
-                best = index
-            }
-        }
-        return refLists[best]
     }
 }
 
