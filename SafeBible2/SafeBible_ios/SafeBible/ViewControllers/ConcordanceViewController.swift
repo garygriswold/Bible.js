86a5284188110c1560e9d7b9750c8894bf6a5b18
Gary N Griswold <gary@shortsands.com>
2019-05-04 09:21:41 -0400

    Refactor ConcordanceViewController

--- SafeBible2/SafeBible_ios/SafeBible/ViewControllers/ConcordanceViewController.swift
+++ SafeBible2/SafeBible_ios/SafeBible/ViewControllers/ConcordanceViewController.swift
@@ -40,7 +40,13 @@ class ConcordanceViewController: AppTableViewController, UITableViewDataSource {
     override func loadView() {
         super.loadView()
         
-        self.navigationItem.title = NSLocalizedString("Word Search", comment: "Title of Search page")
+        if self.section == nil {
+            self.navigationItem.title = NSLocalizedString("Word Search", comment: "Title of Search page")
+        } else {
+            let bible = HistoryModel.shared.currBible
+            let bookId = ConcordanceModel.shared.resultsByBook[self.section!][0].bookId
+            self.navigationItem.title = bible.tableContents?.getBook(bookId: bookId)?.name
+        }
         
         // Along with auto layout, these are the keys for enabling variable cell height
         self.tableView.estimatedRowHeight = 88.0
@@ -160,105 +166,16 @@ class ConcordanceViewController: AppTableViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if self.typeControl.selectedSegmentIndex == ConcordanceViewController.VIEW_LAST_SEARCH {
-            return showLastResult(indexPath: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "concordanceResult", for: indexPath) as! ConcordanceResultCell
+            cell.showLastResult(indexPath: indexPath, section: self.section)
+            return cell
         } else {
-            return showSearchHistory(indexPath: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "concordanceSearch", for: indexPath)
+            cell.contentView.backgroundColor = AppFont.backgroundColor
+            cell.textLabel?.textColor = AppFont.textColor
+            cell.textLabel?.text = ConcordanceModel.shared.getHistory(row: indexPath.row)
+            return cell
         }
-    }
-    
-    private func showSearchHistory(indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "concordanceSearch", for: indexPath)
-        cell.contentView.backgroundColor = AppFont.backgroundColor
-        cell.textLabel?.textColor = AppFont.textColor
-        cell.textLabel?.text = ConcordanceModel.shared.getHistory(row: indexPath.row)
-        return cell
-    }
-    
-    private func showLastResult(indexPath: IndexPath) -> ConcordanceResultCell {
-        let concordance = ConcordanceModel.shared
-        let cell = tableView.dequeueReusableCell(withIdentifier: "concordanceResult", for: indexPath) as! ConcordanceResultCell
-        cell.contentView.backgroundColor = AppFont.backgroundColor
-        
-        cell.title.textColor = AppFont.textColor
-        cell.verse.textColor = AppFont.textColor
-        
-        let bible = HistoryModel.shared.currBible
-
-        if indexPath.row < ConcordanceViewController.VIEW_GROUP_SIZE || self.section != nil {
-            let useSection = (self.section != nil) ? self.section! : indexPath.section
-            let wordRef = concordance.resultsByBook[useSection][indexPath.row]
-            let reference = Reference(bibleId: bible.bibleId, bookId: wordRef.bookId,
-                                      chapter: Int(wordRef.chapter))
-            cell.title.text = reference.description(verse: wordRef.verse)
-            cell.verse.attributedText = self.format(bible: bible, wordRef: wordRef)
-
-        } else {
-            let wordRefGroup = concordance.resultsByBook[indexPath.section]
-            let count = wordRefGroup.count - ConcordanceViewController.VIEW_GROUP_SIZE
-            let bookId = wordRefGroup[0].bookId
-            let name = bible.tableContents?.getBook(bookId: bookId)?.name
-            cell.title.text = "\(count) More In \(name ?? bookId)"
-            cell.verse.text = nil
-        }
-        cell.accessoryType = .disclosureIndicator
-        return cell
-    }
-    
-    private func format(bible: Bible, wordRef: WordRef) -> NSMutableAttributedString? {
-        let pointSize = AppFont.serif(style: .body).pointSize * 0.95
-        if let verseStr = BibleDB.shared.selectVerse(bible: bible, wordRef: wordRef) {
-            let verse = NSMutableAttributedString(string: verseStr)
-            let ranges = self.findRanges(string: verseStr, wordPositions: wordRef.wordPositions!)
-            for range in ranges {
-                verse.addAttribute(.font, value: UIFont.boldSystemFont(ofSize: pointSize), range: range)
-            }
-            return verse
-        } else {
-            return nil
-        }
-    }
-    
-    private func findRanges(string: String, wordPositions: WordPositions) -> [NSRange] {
-        var wordPositions = self.sequencePositions(wordPositions: wordPositions)
-        var ranges = [NSRange]()
-        var search = wordPositions.remove(at: 0)
-        let chars = Array(string)
-        var insideWord = false
-        var wordStart = 0
-        var wordCount: UInt8 = 0
-        for index in 0..<chars.count {
-            let char: Character = chars[index]
-            let unicode = Unicode.Scalar(String(char))
-            if unicode != nil && ConcordanceModel.delims.contains(unicode!) {
-                if wordCount == search && insideWord {
-                    ranges.append(NSRange(location: wordStart, length: index - wordStart))
-                    if wordPositions.count > 0 {
-                        search = wordPositions.remove(at: 0)
-                    } else {
-                        return ranges
-                    }
-                }
-                insideWord = false
-            } else if !char.isPunctuation {
-                if !insideWord {
-                    insideWord = true
-                    wordStart = index
-                    wordCount += 1
-                }
-            }
-        }
-        if wordCount == search && insideWord {
-            ranges.append(NSRange(location: wordStart, length: chars.count - wordStart))
-        }
-        return ranges
-    }
-    
-    private func sequencePositions(wordPositions: WordPositions) -> [UInt8] {
-        var result = [UInt8]()
-        for index in 0..<wordPositions.numWords {
-            result += wordPositions.positions[index]
-        }
-        return result.sorted()
     }
     
     func tableView(_ tableView: UITableView,
@@ -360,5 +277,90 @@ class ConcordanceResultCell : UITableViewCell {
     
     deinit {
         print("**** deinit ConcordanceResultCell ******")
+    }
+    
+    func showLastResult(indexPath: IndexPath, section: Int?) {
+        let concordance = ConcordanceModel.shared
+        self.contentView.backgroundColor = AppFont.backgroundColor
+        
+        self.title.textColor = AppFont.textColor
+        self.verse.textColor = AppFont.textColor
+        
+        let bible = HistoryModel.shared.currBible
+        
+        if indexPath.row < ConcordanceViewController.VIEW_GROUP_SIZE || section != nil {
+            let useSection = (section != nil) ? section! : indexPath.section
+            let wordRef = concordance.resultsByBook[useSection][indexPath.row]
+            let reference = Reference(bibleId: bible.bibleId, bookId: wordRef.bookId,
+                                      chapter: Int(wordRef.chapter))
+            self.title.text = reference.description(verse: wordRef.verse)
+            self.verse.attributedText = self.format(bible: bible, wordRef: wordRef)
+            
+        } else {
+            let wordRefGroup = concordance.resultsByBook[indexPath.section]
+            let count = wordRefGroup.count - ConcordanceViewController.VIEW_GROUP_SIZE
+            let bookId = wordRefGroup[0].bookId
+            let name = bible.tableContents?.getBook(bookId: bookId)?.name
+            self.title.text = "\(count) More In \(name ?? bookId)"
+            self.verse.text = nil
+        }
+        self.accessoryType = .disclosureIndicator
+    }
+    
+    private func format(bible: Bible, wordRef: WordRef) -> NSMutableAttributedString? {
+        let pointSize = AppFont.serif(style: .body).pointSize * 0.95
+        if let verseStr = BibleDB.shared.selectVerse(bible: bible, wordRef: wordRef) {
+            let verse = NSMutableAttributedString(string: verseStr)
+            let ranges = self.findRanges(string: verseStr, wordPositions: wordRef.wordPositions!)
+            for range in ranges {
+                verse.addAttribute(.font, value: UIFont.boldSystemFont(ofSize: pointSize), range: range)
+            }
+            return verse
+        } else {
+            return nil
+        }
+    }
+    
+    private func findRanges(string: String, wordPositions: WordPositions) -> [NSRange] {
+        var wordPositions = self.sequencePositions(wordPositions: wordPositions)
+        var ranges = [NSRange]()
+        var search = wordPositions.remove(at: 0)
+        let chars = Array(string)
+        var insideWord = false
+        var wordStart = 0
+        var wordCount: UInt8 = 0
+        for index in 0..<chars.count {
+            let char: Character = chars[index]
+            let unicode = Unicode.Scalar(String(char))
+            if unicode != nil && ConcordanceModel.delims.contains(unicode!) {
+                if wordCount == search && insideWord {
+                    ranges.append(NSRange(location: wordStart, length: index - wordStart))
+                    if wordPositions.count > 0 {
+                        search = wordPositions.remove(at: 0)
+                    } else {
+                        return ranges
+                    }
+                }
+                insideWord = false
+            } else if !char.isPunctuation {
+                if !insideWord {
+                    insideWord = true
+                    wordStart = index
+                    wordCount += 1
+                }
+            }
+        }
+        if wordCount == search && insideWord {
+            ranges.append(NSRange(location: wordStart, length: chars.count - wordStart))
+        }
+        return ranges
+    }
+    
+    private func sequencePositions(wordPositions: WordPositions) -> [UInt8] {
+        var result = [UInt8]()
+        for index in 0..<wordPositions.numWords {
+            result += wordPositions.positions[index]
+        }
+        return result.sorted()
     }
 }
